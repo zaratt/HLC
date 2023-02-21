49b70355b44751dbf49b013576974fd4010ccfe2
Alan Ribeiro <alandson_ribeiro@hotmail.com>
2023-02-21 09:10:24 -0300

    Hide MongoURI

--- Backend/configs/db-config.js
+++ Backend/configs/db-config.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 
+require('dotenv').config();
 
 const dbConnection = () => {
-    mongoose.connect('mongodb://localhost:27017/colih_app_local')
+    mongoose.connect(process.env.MONGODB_URI)
         .then(() => console.log('Database Connection Successfull'))
         .catch(err => console.log('Failed To Connect With Database, \nReason : ' + err.message))
 }
@@ -14,4 +15,3 @@ module.exports = dbConnection;
 
 
 
-//mongodb+srv://colih_app:YOBSD3RewshmTsIf@cluster1.khyglzb.mongodb.net/colih_db?retryWrites=true&w=majority
